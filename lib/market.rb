b87230bc9511412659dd556a2ae573f7016eb8f8
Yeni <yenicapote08@gmail.com>
2016-09-06 16:48:42 -0700

    adding market.rb with self.all and self.find methods and market_specs.rb with 3 initial tests

--- lib/market.rb
+++ lib/market.rb
@@ -1,17 +1,35 @@
-
+require 'csv'
 module FarMar
   class Market
+    attr_accessor :id, :name, :address, :city, :county, :state, :zip
 
+    def initialize(id, name, address, city, county, state, zip)
+      @id = id
+      @name = name
+      @address = address
+      @city = city
+      @county = county
+      @state = state
+      @zip = zip
+    end
 
+    def self.all
+      info = {}
+      CSV.read('../support/markets.csv').each do |line|
+        id = line[0].to_i
+        name = line[1]
+        address = line[2]
+        city = line[3]
+        county = line[4]
+        state = line[5]
+        zip = line[6]
+        info[id] = self.new(id, name, address, city, county, state, zip)
+      end
+      return info
+    end
 
-
-
-
-
-
-
-
-
-
+    def self.find(id)
+      return self.all[id]
+    end
   end
 end
--- specs/market_spec.rb
+++ specs/market_spec.rb
@@ -1,12 +1,27 @@
 require_relative 'spec_helper'
 
 describe FarMar::Market do
-
   describe "#initialize" do
-    west_seattle = FarMar::Market.new
-
-    it "should create an instance of Market" do
-        west_seattle.must_be_instance_of(FarMar::Market)
+    west_seattle = FarMar::Market.all
+    it "should be able to return Market's name" do
+        first = west_seattle[1]
+        first.name.must_equal("People's Co-op Farmers Market")
     end
   end
+  describe "self.all" do
+    west_seattle = FarMar::Market.all
+
+    it "should return a collection of instances in a hash" do
+        west_seattle.must_be_instance_of(Hash)
+    end
+  end
+
+  describe "self.find(id)" do
+    it "should return an instance of the object matching the id passed" do
+      west_seattle = FarMar::Market.find(2)
+      west_seattle.id.must_equal(2)
+    end
+  end
+
+
 end
