e3525f4692fc2bc23d013755d9a295a6bbcf0146
susan <susan.ra.evans@gmail.com>
2016-09-06 14:29:30 -0700

    specs for reading from file

---
+++ specs/market_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'spec_helper'
+require_relative '../far_mar'
+
+describe 'Market::Testing reading from csv' do
+	it 'must be able to create objects from csv' do
+		markets = FarMar::Market.create_from_file('support/markets.csv')
+		
+		expect(markets.length.must_equal(500))
+		expect(markets[0].name.must_equal("People's Co-op Farmers Market"))
+		expect(markets[345].state.must_equal("Missouri"))
+		expect(markets[499].address.must_equal("111 E. 210th Street"))
+	end
+end
