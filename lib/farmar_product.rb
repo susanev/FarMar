11682aee9e68722bed251f32b6dd3eb9a7c4b75a
susan <susan.ra.evans@gmail.com>
2016-09-06 15:01:47 -0700

    added constructor and self method for reading from csv

--- lib/farmar_product.rb
+++ lib/farmar_product.rb
@@ -3,5 +3,20 @@
 # Vendor_id - (Fixnum) a reference to which vendor sells this product
 
 class FarMar::Product
+	attr_accessor :id, :name, :vendor_id
+
+	def initialize(arr)
+		@id, @name, @vendor_id = arr
+	end
+
+	def self.create_from_file(filename)
+		products = []
+		CSV.foreach(filename) do |row|
+			if row.length > 0 
+				products << FarMar::Product.new(row)
+			end
+		end
+		return products
+	end
 
 end
