0642256156ea101073ec309df811b631a3c26c80
susan <susan.ra.evans@gmail.com>
2016-09-06 14:31:22 -0700

    added method to read file; and instance variables

--- lib/farmar_market.rb
+++ lib/farmar_market.rb
@@ -1,3 +1,25 @@
-class FarMar::Market
+# ID - (Fixnum) a unique identifier for that market
+# Name - (String) the name of the market (not guaranteed unique)
+# Address - (String) street address of the market
+# City - (String) city in which the market is located
+# County - (String) county in which the market is located
+# State - (String) state in which the market is located
+# Zip - (String) zipcode in which the market is located
 
+class FarMar::Market
+	attr_accessor :id, :name, :address, :city, :county, :state, :zip
+
+	def initialize(arr)
+		@id, @name, @address, @city, @county, @state, @zip = arr
+	end
+
+	def self.create_from_file(filename)
+		markets = []
+		CSV.foreach(filename) do |row|
+			if row.length > 0 
+				markets << FarMar::Market.new(row)
+			end
+		end
+		return markets
+	end
 end
--- lib/farmar_vendor.rb
+++ lib/farmar_vendor.rb
@@ -1,3 +1,24 @@
+# ID - (Fixnum) uniquely identifies the vendor
+# Name - (String) the name of the vendor (not guaranteed unique)
+# No. of Employees - (Fixnum) How many employees the vendor has at the market
+# Market_id - (Fixnum) a reference to which market the vendor attends
+
 class FarMar::Vendor
+	attr_accessor :id, :name, :num_employees, :market_id
+
+	def initialize(arr)
+		@id, @name, @num_employees, @market_id = arr
+		@num_employees = @num_employees.to_i
+	end
+
+	def self.create_from_file(filename)
+		vendors = []
+		CSV.foreach(filename) do |row|
+			if row.length > 0 
+				vendors << FarMar::Vendor.new(row)
+			end
+		end
+		return vendors
+	end
 
 end
