521201606785f8d94b5f3fdfa3267d6021d9a12e
susan <susan.ra.evans@gmail.com>
2016-09-06 14:29:45 -0700

    specs for reading from file

---
+++ specs/vendor_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'spec_helper'
+require_relative '../far_mar'
+
+describe 'Vendor::Testing reading from csv' do
+	it 'must be able to create objects from csv' do
+		vendors = FarMar::Vendor.create_from_file('support/vendors.csv')
+		
+		expect(vendors.length.must_equal(2690))
+		expect(vendors[0].name.must_equal("Feil-Farrell"))
+		expect(vendors[279].num_employees.must_equal(7))
+		expect(vendors[2689].market_id.must_equal("500"))
+	end
+end
