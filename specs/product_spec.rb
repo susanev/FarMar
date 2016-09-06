ff4e6a9fd0852523bd6b1307788dc7470fa4ebdd
susan <susan.ra.evans@gmail.com>
2016-09-06 15:00:48 -0700

    added specs for reading from csv

---
+++ specs/product_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'spec_helper'
+require_relative '../far_mar'
+
+describe 'Product::Testing reading from csv' do
+	it 'must be able to create objects from csv' do
+		products = FarMar::Product.create_from_file('support/products.csv')
+		
+		expect(products.length.must_equal(8193))
+		expect(products[0].name.must_equal("Dry Beets"))
+		expect(products[279].id.must_equal("280"))
+		expect(products[8192].vendor_id.must_equal("2690"))
+	end
+end
