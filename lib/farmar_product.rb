e09b88e645f9cc28b5e0125098b852bee1da94c2
susan <susan.ra.evans@gmail.com>
2016-09-06 14:31:50 -0700

    added comments describing csv file

--- lib/farmar_product.rb
+++ lib/farmar_product.rb
@@ -1,3 +1,7 @@
+# ID - (Fixnum) uniquely identifies the product
+# Name - (String) the name of the product (not guaranteed unique)
+# Vendor_id - (Fixnum) a reference to which vendor sells this product
+
 class FarMar::Product
 
 end
--- lib/farmar_sale.rb
+++ lib/farmar_sale.rb
@@ -1,3 +1,9 @@
+# ID - (Fixnum) uniquely identifies the sale
+# Amount - (Fixnum) the amount of the transaction, in cents (i.e., 150 would be $1.50)
+# Purchase_time - (Datetime) when the sale was completed
+# Vendor_id - (Fixnum) a reference to which vendor completed the sale
+# Product_id - (Fixnum) a reference to which product was sold
+
 class FarMar::Sale
 
 end
