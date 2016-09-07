7eb220890f79555f43d74d8398a7c516031985ca
susan <susan.ra.evans@gmail.com>
2016-09-07 11:35:16 -0700

    cleanup

--- specs/vendor_spec.rb
+++ specs/vendor_spec.rb
@@ -19,7 +19,6 @@ describe 'Vendor::Testing reading from csv' do
 		vendors = FarMar::Vendor.by_market(1)
 
 		expect(vendors.length.must_equal(6))
-		puts vendors[5]
 		expect(vendors[5].inspect.must_equal(FarMar::Vendor.new([6, "Zulauf and Sons", 8, 1]).inspect))
 	end
 end
