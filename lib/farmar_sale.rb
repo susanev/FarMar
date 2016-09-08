2ef1ef0d2e60146122d54db615baa68df83e1b85
susan <susan.ra.evans@gmail.com>
2016-09-08 13:53:20 -0700

    added vendor and product instance variables, and removed accessor for vendor_id and product_id; also added stub for self.between

--- lib/farmar_sale.rb
+++ lib/farmar_sale.rb
@@ -7,7 +7,7 @@
 require 'date'
 
 class FarMar::Sale < FarMar::CSV
-	attr_accessor :id, :amount, :purchase_time, :vendor_id, :product_id
+	attr_accessor :id, :amount, :purchase_time, :vendor, :product
 
 	def initialize(arr)
 		@id, @amount, @purchase_time, @vendor_id, @product_id = arr
@@ -16,6 +16,9 @@ class FarMar::Sale < FarMar::CSV
 		@purchase_time = DateTime.parse(@purchase_time)
 		@vendor_id = @vendor_id.to_i
 		@product_id = @product_id.to_i
+
+		@vendor = FarMar::Vendor.find(@vendor_id)
+		@product = FarMar::Product.find(@product_id)
 	end
 
 	def self.all
@@ -24,5 +27,9 @@ class FarMar::Sale < FarMar::CSV
 
 	def self.find(id)
 		return findf(id, all)
+	end
+
+	def self.between(beg_time, end_time)
+
 	end
 end
