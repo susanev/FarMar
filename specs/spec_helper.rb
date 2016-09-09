b8053d07fed539efc2b424439ff34aaf1fae767b
susan <susan.ra.evans@gmail.com>
2016-09-09 13:24:45 -0700

    added simple cov

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,8 +1,10 @@
 #spec helper
+require 'simplecov'
 require 'minitest'
 require 'minitest/spec'
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/pride'
 
+SimpleCov.start
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
