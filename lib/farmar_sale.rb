a7ff9602cb55e61aaaf781add97a14cc9623b26d
susan <susan.ra.evans@gmail.com>
2016-09-09 14:42:59 -0700

    cleaned up

--- lib/farmar_sale.rb
+++ lib/farmar_sale.rb
@@ -19,6 +19,14 @@ class FarMar::Sale #< FarMar::CSV
 		@product_id = @product_id.to_i
 	end
 
+	def vendor
+		return FarMar::Vendor.find(@vendor_id)
+	end
+
+	def product
+		return FarMar::Product.find(@product_id)
+	end
+
 	def self.all
 		#return allf("support/sales.csv")
 		return FarMar::CSV.all("support/sales.csv", FarMar::Sale)
@@ -29,21 +37,14 @@ class FarMar::Sale #< FarMar::CSV
 		return FarMar::CSV.find(id, all)
 	end
 
-	def vendor
-		return FarMar::Vendor.find(@vendor_id)
-	end
-
-	def product
-		return FarMar::Product.find(@product_id)
-	end
-
 	def self.between(beg_time, end_time)
 		sales = []
 		all.each do |sale|
-			time = sale.purchase_time.strftime("%H:%M")
-			if  time > beg_time && time < end_time
+			#time = sale.purchase_time.strftime("%H:%M")
+			if  sale.purchase_time > beg_time && sale.purchase_time < end_time
 				sales << sale
 			end
 		end
+		return sales
 	end
 end
