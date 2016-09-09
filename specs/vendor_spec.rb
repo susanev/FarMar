0b019b15e93e8e3a47f5ff0448941ee340352ea5
susan <susan.ra.evans@gmail.com>
2016-09-09 13:12:25 -0700

    added specs for products, sales, and revenue methods

--- specs/vendor_spec.rb
+++ specs/vendor_spec.rb
@@ -21,4 +21,49 @@ describe 'Vendor::Testing reading from csv' do
 		expect(vendors.length.must_equal(6))
 		expect(vendors[5].must_equal(FarMar::Vendor.new([6, "Zulauf and Sons", 8, 1])))
 	end
+
+	it 'must return Market assoicated with vendor' do
+		vendors = FarMar::Vendor.by_market(2)
+		market = FarMar::Market.find(2)
+
+		vendors.each do |vendor|
+			expect(vendor.market_id.must_equal(market.id))
+		end
+	end
+
+	it 'must return products associated with vendor' do
+		vendor = FarMar::Vendor.find(1)
+
+		vendor.products.each do |product|
+			expect(product.vendor_id.must_equal(vendor.id))
+		end
+	end
+
+	it 'must return sales associated with vendor' do
+		vendor = FarMar::Vendor.find(2)
+		
+		vendor.sales.each do |sale|
+			expect(sale.vendor_id.must_equal(vendor.id))
+		end
+	end
+
+	it 'must calculate revenue of sales correctly' do
+		vendor = FarMar::Vendor.find(3)
+		expect(vendor.revenue.must_equal(40126))
+
+		vendor = FarMar::Vendor.find(4)
+		expect(vendor.revenue.must_equal(26866))
+	end
+
 end
+
+
+
+
+
+
+
+
+
+
+
